1ea33c3502e044ad5c03ba068986173aa83e9b6e
Aditya <Adityakasera66@gmail.com>
2026-03-03 23:18:03 +0530

    Add Limit less then 0 And less than 10

--- react/counter/src/App.jsx
+++ react/counter/src/App.jsx
@@ -2,29 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App(){
-     let [add,setAdd]=useState(6);
+     let [add,setAdd]=useState(10);
   
   
-
-    function Add(){
-      setAdd(add+1)
-    }
-    
+function Add(){
+  if(add < 10){
+    setAdd(add + 1);
+  }
+}
 function remove(){
   if(add>0){
   setAdd(add-1)
 }
 }
-  
+
   return (
     <>
     <h1> Counter in React </h1>
     <button onClick={Add}> Add Count : {add}</button> <br /> <br />
    
-<button onClick={remove} disabled={add===0}>
-    Decrease
-
-</button>
+<button onClick={remove} disabled={add===0}> Decrease</button> <br/>
+ <br />
+ 
+<button onClick={Add} disabled={add >= 10}>Increase</button>
     </>
   )
 }
