b33caca0dff44e90406038eceb01382c91311d52
Aditya <Adityakasera66@gmail.com>
2026-03-05 19:48:02 +0530

    image div

--- GSAP/cursor/cursor.js
+++ GSAP/cursor/cursor.js
@@ -1,5 +1,6 @@
 let main=document.querySelector("#main");
 let cursor=document.querySelector("#cursor");
+let imageDiv=document.querySelector("#image")
 
 main.addEventListener("mousemove",function(dets){
  gsap.to(cursor,{
@@ -8,4 +9,20 @@ main.addEventListener("mousemove",function(dets){
     duration:1,
     ease:"back.out"
  })
+})
+
+imageDiv.addEventListener("mouseenter",function(){
+   gsap.to(cursor,{
+      scale:2
+   })
+})
+imageDiv.addEventListener("mouseenter",function(){
+   gsap.to(cursor,{
+      scale:2
+   })
+})
+imageDiv.addEventListener("mouseleave",function(){
+   gsap.to(cursor,{
+      scale:1
+   })
 })
