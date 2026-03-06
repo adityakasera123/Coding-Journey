a853a16db81072220e192d8d890f330f9cc62690
Aditya <Adityakasera66@gmail.com>
2026-03-06 22:59:45 +0530

    Add OPEN animation

--- GSAP/textAnimation/text.js
+++ GSAP/textAnimation/text.js
@@ -2,23 +2,36 @@ function breakTheText(){
 let h1=document.querySelector("h1")
 let h1text=h1.textContent
 
-
 let splittedText=h1text.split("");
+
+let halfValue=splittedText.length/2
 
 let clutter=""
 
-splittedText.forEach(function(dets){
-    clutter +=`<span> ${dets} </span>`
- 
+splittedText.forEach(function(dets,indx){
+    if(indx<halfValue){
+
+          clutter +=`<span class="a"> ${dets} </span>`
+    }
+    else{
+    clutter +=`<span class="b"> ${dets} </span>`
+    }
 })
 h1.innerHTML=clutter
 }
 breakTheText();
 
-gsap.from("h1 span",{
-    y:50,
+gsap.from("h1 .a",{
+    y:80,
     opacity:0,
     duration:0.8,
     delay:0.5,
     stagger:0.15
+})
+gsap.from("h1 .b",{
+    y:80,
+    opacity:0,
+    duration:0.8,
+    delay:0.5,
+    stagger:-0.15
 })
