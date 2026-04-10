566bb200c19b7bdf1fe4162564c03cc2a0d73f4d
Aditya <adityakasera40@gmail.com>
2026-04-10 14:45:27 +0530

    Hardcoated Card next dynamically added

---
+++ react/02-cards-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './components/card'
+
+function App(){
+  return (
+    <div className='parent'>
+
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+export default App;
---
+++ react/02-cards-project/src/components/card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const card = () => {
+  return (
+     <div className="card">
+       <div>
+         <div className="top">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7kkJwcOdJzQY1qwZO9N89pgwRhyHdvI3kFg&s" alt="" />
+          <button>Save <i class="ri-bookmark-line"></i></button>
+        </div>
+        <div className="center">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/UX Desinger </h2>
+          <div className="centerflex">
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+      
+        </div>
+       </div>
+        <div className="bottom">
+          
+            <div className="bottom1">
+              <h3>$200-250k</h3>
+              <p>Banglore,India</p>
+            </div>
+          
+            <button>Apply Now</button>
+        </div>
+      </div>
+  )
+}
+
+export default card
