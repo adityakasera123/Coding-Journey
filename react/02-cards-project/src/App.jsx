da010101ddb92733daef16895d69362bc2c916d5
Aditya <adityakasera40@gmail.com>
2026-04-15 19:36:10 +0530

    created data in json

--- react/02-cards-project/src/App.jsx
+++ react/02-cards-project/src/App.jsx
@@ -1,12 +1,114 @@
 import Card from './components/card'
 
 function App(){
+const jobOpenings = [
+  {
+    brandLogo: "https://logo.clearbit.com/google.com",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/apple.com",
+    companyName: "Apple",
+    datePosted: "1 week ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$60/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/meta.com",
+    companyName: "Meta",
+    datePosted: "3 days ago",
+    post: "React Developer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$40/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/amazon.com",
+    companyName: "Amazon",
+    datePosted: "2 weeks ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$55/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/netflix.com",
+    companyName: "Netflix",
+    datePosted: "6 days ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$65/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/microsoft.com",
+    companyName: "Microsoft",
+    datePosted: "10 days ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$58/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/adobe.com",
+    companyName: "Adobe",
+    datePosted: "4 days ago",
+    post: "UI/UX Developer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$42/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/uber.com",
+    companyName: "Uber",
+    datePosted: "3 weeks ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$62/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/oracle.com",
+    companyName: "Oracle",
+    datePosted: "8 days ago",
+    post: "Java Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$50/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/salesforce.com",
+    companyName: "Salesforce",
+    datePosted: "12 days ago",
+    post: "DevOps Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$57/hr",
+    location: "Mumbai, India"
+  }
+];
+console.log(jobOpenings);
   return (
     <div className='parent'>
-
-      <Card />
-      <Card />
-      <Card />
+     {jobOpenings.map(function(elem){
+      return <Card company={elem.companyName} post={elem.post} date={elem.datePosted}/>
+     })}
     </div>
   )
 }
--- react/02-cards-project/src/components/card.jsx
+++ react/02-cards-project/src/components/card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const card = () => {
+const card = (props) => {
+
   return (
      <div className="card">
        <div>
@@ -9,8 +10,8 @@ const card = () => {
           <button>Save <i class="ri-bookmark-line"></i></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Desinger </h2>
+          <h3>{props.company} <span>{props.date}</span></h3>
+          <h2>{props.post} </h2>
           <div className="centerflex">
             <h4>Part Time</h4>
             <h4>Senior Level</h4>
