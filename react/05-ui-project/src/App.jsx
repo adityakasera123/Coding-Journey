39f8b2fffbbc5fd1855e21a3a77bcf762a546cae
Aditya <adityakasera40@gmail.com>
2026-04-17 22:10:19 +0530

    start ui project through react

---
+++ react/05-ui-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from './components/Section2/Section2'
+import Section1 from './components/Section1/Section1'
+
+const App = () => {
+  return (
+    <div>
+      <Section1 />
+      <Section />
+    </div>
+  )
+}
+
+export default App
---
+++ react/05-ui-project/src/components/Section2/Section2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div className='w-full h-screen bg-amber-700'>Section</div>
+  )
+}
+
+export default Section
