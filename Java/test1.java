bbd659a64cc6ec5b78d35efbd200d903acb51c51
Aditya <adityakasera40@gmail.com>
2026-06-17 23:00:33 +0530

    create java code

---
+++ Java/test1.java
@@ -0,0 +1,26 @@
+class Student{
+    private String name;
+    private int age;
+    
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    String getName(){
+        return this.name;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+    int getAge(){
+        return this.age;
+    }
+    
+    void setAge(int age){
+        this.age=age;
+    }
+  
+    
+}
+
