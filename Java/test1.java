c0b104aebbaafe8a51d9e80f52faf0ab7a39d1ac
Aditya <adityakasera40@gmail.com>
2026-06-18 23:10:57 +0530

    linked list best question

--- Java/test1.java
+++ Java/test1.java
@@ -24,3 +24,16 @@ class Student{
     
 }
 
+public class test1{
+    public static void main(String[] args){
+        Student s1 = new Student("Golu",25);
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+        
+        s1.setAge(20);
+        System.out.println(s1.getAge());
+        
+        s1.setName("aditya");
+        System.out.println(s1.getName());
+    }
+}
